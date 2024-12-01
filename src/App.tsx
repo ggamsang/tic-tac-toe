3530c105b397e02f33047d1bf5e1f51dacdcf35b
ggamsang <w.ggmsng@gmail.com>
2024-12-01 21:17:29 +0900

    타입이 적용된 틱택토

--- src/App.tsx
+++ src/App.tsx
@@ -61,22 +61,43 @@ function Board({ xIsNext, squares, onPlay }: BoardProps) {
 }
 
 export default function Game() {
-  const [xIsNext, setXIsNext] = useState(true)
   const [history, setHistory] = useState([Array(9).fill(null)])
-  const currentSquares = history[history.length - 1]
+  const [currentMove, setCurrentMove] = useState(0)
+  // const [xIsNext, setXIsNext] = useState(true)
+  const xIsNext: boolean = currentMove % 2 === 0
+  const currentSquares: (string | null)[] = history[currentMove]
 
   function handlePlay(nextSquares: (string | null)[]) {
-    setHistory([...history, nextSquares])
-    setXIsNext(!xIsNext)
+    const nextHistory: (string | null)[][] = [...history.slice(0, currentMove + 1), nextSquares]
+    setHistory(nextHistory)
+    setCurrentMove(nextHistory.length - 1)
+    // setXIsNext(!xIsNext)
   }
 
+  function jumpTo(nextMove: number) {
+    setCurrentMove(nextMove)
+    // setXIsNext(nextMove % 2 === 0)
+  }
+  const moves = history.map((squares: (string | null)[], move: number) => {
+    let description: string = ""
+    if (move > 0)
+      description = 'Go to move #' + move
+    else
+      description = 'Go to game start'
+    return <li key={move}>
+      <button onClick={() => jumpTo(move)}>{description}</button>
+    </li>
+  })
   return (
     <div className="game">
       <div className="game-board">
-        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
+        <Board
+          xIsNext={xIsNext} squares={currentSquares}
+          onPlay={handlePlay}
+        />
       </div>
       <div className="game-info">
-        <ol>{/*TODO*/}</ol>
+        <ol>{moves}</ol>
       </div>
     </div>
   );
